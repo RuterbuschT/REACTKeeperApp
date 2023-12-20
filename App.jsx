090862c46ca9532cd5498c74a54c1433ead05269
Tyler Ruterbusch <139671182+RuterbuschT@users.noreply.github.com>
2023-12-20 00:35:02 -0500

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  };
+
+  const deleteNote = (id) => {
+    setNotes((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return !index == id;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.index}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
